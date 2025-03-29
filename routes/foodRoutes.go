d4c371520e74d4ebb287d8290374f367d1d52a6f
harsh.backend <harsh.2024it1034@kiet.edu>
2025-03-29 10:32:41 +0530

    Added aunthetication in subsequent requests

--- routes/foodRoutes.go
+++ routes/foodRoutes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"golang_restaurant_backend/controllers"
+	"golang_restaurant_backend/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,9 +10,8 @@ import (
 func FoodRoutes(router *gin.Engine) {
 	food := router.Group("api/v1/food")
 	{
-		food.POST("/createFood", controllers.CreateFood())
-		food.GET("/getFood/:foodID", controllers.GetFood())
-		food.GET("/getAllFoods", controllers.GetAllFoods())
+		food.POST("/createFood", middlewares.Auntheticate(), controllers.CreateFood())
+		food.GET("/getFood/:foodID", middlewares.Auntheticate(), controllers.GetFood())
+		food.GET("/getAllFoods", middlewares.Auntheticate(), controllers.GetAllFoods())
 	}
 }
-
--- routes/orderRoutes.go
+++ routes/orderRoutes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"golang_restaurant_backend/controllers"
+	"golang_restaurant_backend/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,8 +10,8 @@ import (
 func Order(router *gin.Engine) {
 	Order := router.Group("api/v1")
 	{
-		Order.POST("/order", controllers.Order())
-		Order.GET("/getOrder/:order_id", controllers.GetOrder())
-		Order.GET("/getAllOrders", controllers.GetAllOrders())
+		Order.POST("/order", middlewares.Auntheticate(), controllers.Order())
+		Order.GET("/getOrder/:order_id", middlewares.Auntheticate(), controllers.GetOrder())
+		Order.GET("/getAllOrders", middlewares.Auntheticate(), controllers.GetAllOrders())
 	}
 }
